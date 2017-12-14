4bba1a6c1a63eb049ab4eb50e07d92cc7106bd4c
Shailesh K. Jha <j.shailesh@gmail.com>
2017-12-14 18:36:16 -0500

    Add files via upload

--- notebook.py
+++ notebook.py
@@ -41,20 +41,29 @@ class Notebook:
     def modify_memo(self, note_id, memo):
         '''Find a note with the given id and change its
         memo to the given value.'''
-        for note in self.notes:
-            if note.id == note_id:
-                note.memo = memo
-                break
+        note = self._find_note(note_id)
+        if note:
+            note.memo = memo
+            return True
+        return False
 
     def modify_tags(self, note_id, tags):
         '''Find the note with the given id and change its
         tags to the given value.'''
-        for note in self.notes:
-            if note.id == note_id:
-                note.tags = tags
-                break
-
+        note = self._find_note(note_id)
+        if note:
+            note.tags = tags
+            return True
+        return False
+        
     def search(self, filter):
         '''Find all notes that match the given filter string'''
         return [note for note in self.notes if 
                 note.match(filter)]
+
+    def _find_note(self, note_id):
+        '''Locate the note with the given id.'''
+        for note in self.notes:
+            if str(note.id) == str(note_id):
+                return note
+        return None
